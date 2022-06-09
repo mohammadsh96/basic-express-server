0d209903a0c319a64433b8a5e23fb6c64b8dfa9b
mohammadsh96 <mhmd.shrydh1996@gmail.com>
2022-06-09 15:14:56 +0300

    update the 500 error responding

--- error-handlers/500.js
+++ error-handlers/500.js
@@ -1,8 +1,19 @@
 'use strict';
 module.exports = (error, req, res, next) => {
+  if (req.query.name === '') {
     res.status(500).send({
-        code: 500,
-        route: req.path,
-        message: `Server Error:${error.message}`,
-    });
+      code: 500,
+      route: req.path,
+      message: `Server Error:${error} and you entered an empty name please don't do it again`,
+    })
+  } else if (req.query.name === 'wrong Name') {
+    res.status(500).send({
+      code: 500,
+      route: req.path,
+      message: `Server Error:${error} and  you enterd a number `,
+    })
+  };
 };
+
+
+
